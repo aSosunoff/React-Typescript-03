a7191634be6eded319c06e2a3f778fa5e44735bf
asosunoff <al.sosunoff@gmail.com>
2021-01-12 20:36:38 +0300

    fix style

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -5,9 +5,15 @@ import styles from "./ItemList.module.scss";
 const ItemList: React.FC = () => {
   return (
     <ul className={cn("list-group", styles["item-list"])}>
-      <li className={styles["list-group-item"]}>Luke Skywalker</li>
-      <li className={styles["list-group-item"]}>Darth Vader</li>
-      <li className={styles["list-group-item"]}>R2-D2</li>
+      <li className={cn("list-group-item", styles["list-group-item"])}>
+        Luke Skywalker
+      </li>
+      <li className={cn("list-group-item", styles["list-group-item"])}>
+        Darth Vader
+      </li>
+      <li className={cn("list-group-item", styles["list-group-item"])}>
+        R2-D2
+      </li>
     </ul>
   );
 };
