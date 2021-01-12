3e8aa715f03b6fbf29419866de54f03ee16de33d
asosunoff <al.sosunoff@gmail.com>
2021-01-12 20:28:31 +0300

    random planet fix

--- src/components/RandomPlanet/RandomPlanet.tsx
+++ src/components/RandomPlanet/RandomPlanet.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-/* import styles from "./RandomPlanet.module.scss"; */
+import cn from "classnames";
+import styles from "./RandomPlanet.module.scss";
 
 const RandomPlanet: React.FC = () => {
-  return <div>RandomPlanet</div>;
+  return (
+    <div className={cn("jumbotron rounded", styles["random-planet"])}>
+      <img
+        className={styles["planet-image"]}
+        alt="img"
+        src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
+      />
+      <div>
+        <h4>Planet Name</h4>
+        <ul className={cn("list-group-flush", styles["list-group"])}>
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Population</span>
+            <span>123124</span>
+          </li>
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Rotation Period</span>
+            <span>43</span>
+          </li>
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Diameter</span>
+            <span>100</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default RandomPlanet;
