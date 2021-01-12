39516848b26552a94e72804472f4cb31797ed832
asosunoff <al.sosunoff@gmail.com>
2021-01-12 20:14:57 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,24 @@ import React from "react";
 /* import styles from "./Header.module.scss"; */
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header d-flex">
+      <h3>
+        <a href="/#">Star DB</a>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <a href="/#">People</a>
+        </li>
+        <li>
+          <a href="/#">Planets</a>
+        </li>
+        <li>
+          <a href="/#">Starships</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
