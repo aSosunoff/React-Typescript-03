ec666e09e625249e6934a81c106696a83252d5b1
asosunoff <al.sosunoff@gmail.com>
2021-01-12 15:23:37 +0300

    project is cleared

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./App.module.scss";
+import cn from "classnames";
+
+const App: React.FC = () => {
+  return <div className={cn(styles.app)}></div>;
+};
+
+export default App;
