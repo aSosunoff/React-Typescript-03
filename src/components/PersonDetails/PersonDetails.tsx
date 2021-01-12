ecefe2a6c875510f528ff1bae879ae3ceec55c3d
asosunoff <al.sosunoff@gmail.com>
2021-01-12 20:24:12 +0300

    fix person details

--- src/components/PersonDetails/PersonDetails.tsx
+++ src/components/PersonDetails/PersonDetails.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-/* import styles from "./PersonDetails.module.scss"; */
+import cn from "classnames";
+import styles from "./PersonDetails.module.scss";
 
 const PersonDetails: React.FC = () => {
-  return <div>PersonDetails</div>;
+  return (
+    <div className={cn("card", styles["person-details"])}>
+      <img
+        className={styles["person-image"]}
+        alt="img"
+        src="https://starwars-visualguide.com/assets/img/characters/3.jpg"
+      />
+
+      <div className="card-body">
+        <h4>R2-D2</h4>
+        <ul className="list-group list-group-flush">
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Gender</span>
+            <span>male</span>
+          </li>
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Birth Year</span>
+            <span>43</span>
+          </li>
+          <li className={styles["list-group-item"]}>
+            <span className={styles["term"]}>Eye Color</span>
+            <span>red</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetails;
