81d2f3fc3ee028a6a6c2e7a845922253dfc43597
asosunoff <al.sosunoff@gmail.com>
2021-01-13 11:44:36 +0300

    Added ErrorBoundaryIndicator

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,42 +2,34 @@ import React, { useState } from "react";
 import cn from "classnames";
 import Header from "../Header";
 import RandomPlanet from "../RandomPlanet";
-import ItemList from "../ItemList";
-import PersonDetails from "../PersonDetails";
-import { PersonProvider } from "../../context/personContext";
 import styles from "./App.module.scss";
 import ErrorButton from "../ErrorButton";
+import PeoplePage from "../PeoplePage";
+import ErrorBoundaryIndicator from "../ErrorBoundaryIndicator";
 
 const App: React.FC = () => {
   const [showRandomPlanet, setShowRandomPlanet] = useState(true);
 
   return (
-    <div className={cn("container", styles["stardb-app"])}>
-      <Header />
+    <ErrorBoundaryIndicator>
+      <div className={cn("container", styles["stardb-app"])}>
+        <Header />
 
-      {showRandomPlanet ? <RandomPlanet /> : null}
+        {showRandomPlanet ? <RandomPlanet /> : null}
 
-      <div className={cn("row mb2", styles["button-row"])}>
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={() => setShowRandomPlanet((prev) => !prev)}
-        >
-          Toggle Random Planet
-        </button>
-        <ErrorButton />
-      </div>
-
-      <PersonProvider>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails />
-          </div>
+        <div className={cn("row mb2", styles["button-row"])}>
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={() => setShowRandomPlanet((prev) => !prev)}
+          >
+            Toggle Random Planet
+          </button>
+          <ErrorButton />
         </div>
-      </PersonProvider>
-    </div>
+
+        <PeoplePage />
+      </div>
+    </ErrorBoundaryIndicator>
   );
 };
 
