26b41a57713ac586c0076159926059f192eba880
asosunoff <al.sosunoff@gmail.com>
2021-01-13 09:44:48 +0300

    fix

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback } from "react";
 import cn from "classnames";
 import styles from "./ItemList.module.scss";
 import { useAllPeople } from "../../utils/SwapiService";
