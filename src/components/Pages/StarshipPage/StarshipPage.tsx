7d1555efa32db37514ebbe7b8b8f67b3a77dc534
asosunoff <al.sosunoff@gmail.com>
2021-01-14 12:13:59 +0300

    fix

--- src/components/Pages/StarshipPage/StarshipPage.tsx
+++ src/components/Pages/StarshipPage/StarshipPage.tsx
@@ -23,7 +23,7 @@ const StarshipPage: React.FC<BasePageType> = ({ className, style }) => {
       className={className}
       style={style}
       renderLeft={() =>
-        showSpinnerList ? null : (
+        showSpinnerList || (
           <ItemList<StarshipType>
             list={results}
             setId={setId}
@@ -32,18 +32,17 @@ const StarshipPage: React.FC<BasePageType> = ({ className, style }) => {
         )
       }
       renderRight={() =>
-        showSpinnerDetails
-          ? null
-          : data && (
-              <Details
-                title={data?.name || ""}
-                imgUrl={`https://starwars-visualguide.com/assets/img/starships/${data.id}.jpg`}
-              >
-                <Record title="Model" text={data.model} />
-                <Record title="Length" text={data.length} />
-                <Record title="Cost" text={data.cost_in_credits} />
-              </Details>
-            )
+        showSpinnerDetails ||
+        (data && (
+          <Details
+            title={data?.name || ""}
+            imgUrl={`https://starwars-visualguide.com/assets/img/starships/${data.id}.jpg`}
+          >
+            <Record title="Model" text={data.model} />
+            <Record title="Length" text={data.length} />
+            <Record title="Cost" text={data.cost_in_credits} />
+          </Details>
+        ))
       }
     />
   );
