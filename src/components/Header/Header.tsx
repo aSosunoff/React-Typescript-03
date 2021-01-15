9093d31a987e74e6a29c1a487d5e1242356dd3e0
asosunoff <al.sosunoff@gmail.com>
2021-01-16 00:42:33 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,32 +6,36 @@ import { useSwapiServiceContext } from "../../context/SwapiServiceContext";
 const Header: React.FC = () => {
   const { toggleService, nameService } = useSwapiServiceContext();
   return (
-    <div className={cn("d-flex", styles.header)}>
-      <h3>
-        <a href="/#">Star DB</a>
-      </h3>
-      <ul className="d-flex">
-        <li>
-          <a href="/#">People</a>
-        </li>
-        <li>
-          <a href="/#">Planets</a>
-        </li>
-        <li>
-          <a href="/#">Starships</a>
-        </li>
-      </ul>
+    <div className="row">
+      <div className="col">
+        <div className={cn("d-flex", styles.header)}>
+          <h3>
+            <a href="/#">Star DB</a>
+          </h3>
+          <ul className="d-flex">
+            <li>
+              <a href="/#">People</a>
+            </li>
+            <li>
+              <a href="/#">Planets</a>
+            </li>
+            <li>
+              <a href="/#">Starships</a>
+            </li>
+          </ul>
 
-      <button
-        onClick={toggleService}
-        className="btn btn-primary btn-sm"
-        style={{
-          height: "2.2rem",
-          marginTop: "3px",
-        }}
-      >
-        Change Service ( current {nameService} )
-      </button>
+          <button
+            onClick={toggleService}
+            className="btn btn-primary btn-sm"
+            style={{
+              height: "2.2rem",
+              marginTop: "3px",
+            }}
+          >
+            Change Service ( current {nameService} )
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
