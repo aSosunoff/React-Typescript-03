a021e7c519f26c5a123d0e827bfb9a6228e2a8c5
asosunoff <al.sosunoff@gmail.com>
2021-01-17 18:35:06 +0300

    added pages

--- src/Enums/Pages.ts
+++ src/Enums/Pages.ts
@@ -3,10 +3,13 @@ export enum Pages {
 
     PEOPLE = '/people/',
     PEOPLE_ID = '/people/:id',
+    PEOPLE_ID_Q = '/people/:id?',
 
     PLANET = '/planet/',
     PLANET_ID = '/planet/:id',
+    PLANET_ID_Q = '/planet/:id?',
 
     STARSHIP = '/starship/',
     STARSHIP_ID = '/starship/:id',
+    STARSHIP_ID_Q = '/starship/:id?',
 }
