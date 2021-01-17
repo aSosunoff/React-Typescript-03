b1bd7ec85f06227af310a575ac696b04bba3a638
asosunoff <al.sosunoff@gmail.com>
2021-01-17 18:14:14 +0300

    delete

--- src/components/StarComponents/StarComponents.tsx
+++
@@ -1,6 +0,0 @@
-export { PeopleList } from "./PeopleList";
-export { PlanetList } from "./PlanetList";
-export { StarshipList } from "./StarshipList";
-export { PeopleDetails } from "./PeopleDetails";
-export { PlanetDetails } from "./PlanetDetails";
-export { StarshipDetails } from "./StarshipDetails";
