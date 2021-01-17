c077d2be99205518aff77ca1de90a2e8b9ee18c3
asosunoff <al.sosunoff@gmail.com>
2021-01-17 23:33:13 +0300

    fix head component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,57 +9,65 @@ import { Pages } from "../../Enums/Pages";
 const Header: React.FC = ({ children }) => {
   const { toggleService, nameService } = useSwapiServiceContext();
   return (
-    <div className="row">
-      <div className="col">
-        <div className={cn("d-flex", styles.header)}>
-          <h3>
-            <Link to={Pages.HOME}>Star DB</Link>
-          </h3>
+    <>
+      <div className="row">
+        <div className="col">
+          <div className={cn("d-flex", styles.header)}>
+            <h3>
+              <Link to={Pages.HOME}>Star DB</Link>
+            </h3>
 
-          <ul className="d-flex">
-            <li>
-              <Link to={Pages.PEOPLE}>People</Link>
-            </li>
-            <li>
-              <Link to={Pages.PLANET}>Planets</Link>
-            </li>
-            <li>
-              <Link to={Pages.STARSHIP}>Starships</Link>
-            </li>
-            <li>
-              <Link to={Pages.LOGIN}>Login</Link>
-            </li>
-            <li>
-              <Link to={Pages.SECRET}>Secret</Link>
-            </li>
-          </ul>
+            <ul className="d-flex">
+              <li>
+                <Link to={Pages.PEOPLE}>People</Link>
+              </li>
+              <li>
+                <Link to={Pages.PLANET}>Planets</Link>
+              </li>
+              <li>
+                <Link to={Pages.STARSHIP}>Starships</Link>
+              </li>
+              <li>
+                <Link to={Pages.LOGIN}>Login</Link>
+              </li>
+              <li>
+                <Link to={Pages.SECRET}>Secret</Link>
+              </li>
 
-          <button
-            onClick={toggleService}
-            className="btn btn-primary btn-sm"
-            style={{
-              height: "2.2rem",
-              marginTop: "3px",
-            }}
-          >
-            Change Service ( current {nameService} )
-          </button>
-
-          <ErrorButton
-            style={{
-              height: "2.2rem",
-              marginTop: "3px",
-            }}
-          />
-
-          <ul className="d-flex" style={{ paddingLeft: 0 }}>
-            {React.Children.map(children, (child) => (
-              <li>{React.cloneElement(child as any)}</li>
-            ))}
-          </ul>
+              <ul className="d-flex" style={{ paddingLeft: 0 }}>
+                {React.Children.map(children, (child) => (
+                  <li>{React.cloneElement(child as any)}</li>
+                ))}
+              </ul>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="row">
+        <div className="col">
+          <div className={cn("d-flex", styles.header)}>
+            <button
+              onClick={toggleService}
+              className="btn btn-primary btn-sm"
+              style={{
+                height: "2.2rem",
+                marginTop: "3px",
+              }}
+            >
+              Change Service ( current {nameService} )
+            </button>
+
+            <ErrorButton
+              style={{
+                height: "2.2rem",
+                marginTop: "3px",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
